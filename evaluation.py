f7112d09595ba4a0c0d182f5e89dbfc887a0670f
Chris Tralie <chris.tralie@gmail.com>
2022-08-05 23:35:02 -0400

    Adding some evaluation code for the UCR dataset

---
+++ evaluation.py
@@ -0,0 +1,143 @@
+import time
+import numpy as np
+import argparse
+from mergetree import *
+from matching import *
+from matching_plots import *
+from dtw import *
+from utils import *
+from evaluation import *
+import pyts
+import pyts.datasets
+import time
+import gudhi # For bottleneck
+import scipy.io as sio
+import os
+from sys import argv
+
+def euclidean_compare(x, y):
+    N = max(len(x), len(y))
+    # Zeropadding, as suggested by Eammon
+    if len(x) < N:
+        x = np.concatenate((x, np.zeros(N-len(x))))
+    if len(y) < N:
+        y = np.concatenate((y, np.zeros(N-len(y))))
+    return np.sum((x-y)**2)
+
+def get1nn_error_rate(D, idx_train, idx_test):
+    """
+    Compute the error rate of a 1 nearest neighbor classifier
+    on a particular dataset
+
+    Parameters
+    ----------
+    D: ndarray(M, N)
+        Distances to examine
+    idx_train: ndarray(M)
+        Class indices of the training set
+    idx_test: ndarray(N)
+        Class indices of the test set
+    """
+    correct = 0
+    i = np.argmin(D, axis=0)
+    correct = np.sum(idx_train[i] == idx_test)
+    N = D.shape[1]
+    return (N-correct)/N
+
+def get_dataset(dataset_name, all_eps, circular=False):
+    """
+    Load in a UCR dataset and create all merge trees and simplifications
+
+    Parameters
+    ----------
+    dataset_name: string
+        Name of UCR dataset
+    all_eps: ndarray(K)
+        List of all epsilon simplifications to make
+    circular: boolean
+        Whether to use circular boundary conditions for sublevelset filtrations
+    
+    Returns
+    -------
+    dataset: dict
+        UCR dataset
+    """
+    dataset = pyts.datasets.fetch_ucr_dataset(dataset_name)
+    for s in ["train", "test"]:
+        idx = np.argsort(dataset['target_'+s])
+        #dataset['data_'+s] = [znormalize(x) for x in dataset['data_'+s][idx]]
+        dataset['data_'+s] = [x[~np.isnan(x)] for x in dataset['data_'+s][idx]]
+        for i, x in enumerate(dataset['data_'+s]):
+            MT = MergeTree()
+            MT.init_from_timeseries(x, circular=circular)
+            dataset['data_'+s][i] = (MT, x)
+        for eps in all_eps:
+            skey = 'data_{}_{:.1f}'.format(s, eps)
+            dataset[skey] = []
+            for x in dataset['data_'+s]:
+                MT = MergeTree()
+                MT.init_from_timeseries(x[1], circular=circular)
+                MT.persistence_simplify(eps)
+                dataset[skey].append((MT, MT.get_rep_timeseries()['ys']))
+    return dataset
+
+
+def get_dataset_distances(dataset_name, dataset, methods, prefix="."):
+    """
+    Compute all pairwise distances between the union of training and test data
+    for a particular dataset
+
+    Parameters
+    ----------
+    dataset_name: string
+        Name of the dataset
+    dataset: dictionary
+        Information with the dataset
+    methods: dictionary {string: function handle}
+        Methods to compute distances
+    prefix: string
+        Path to results file
+    """
+    M = len(dataset['data_train'])
+    N = len(dataset['data_test'])
+    for method_name, method in methods.items():
+        for eps in all_eps:
+            XAll = dataset['data_train_{}'.format(eps)] + dataset['data_test_{}'.format(eps)]
+            eps = "{:.1f}".format(eps)
+            tic = time.time()
+            D = np.zeros((M+N, M+N))
+            filename = "{}/{}_{}_{}.mat".format(prefix, dataset_name, method_name, eps)
+            if os.path.exists(filename):
+                print("Skipping", filename)
+            else:
+                print("\nTraining ", method_name, "on", dataset_name, ", eps = ", eps)
+                for i in range(M+N):
+                    if i%10 == 0 and i != 0:
+                        print(".", end="")
+                    xi = XAll[i]
+                    for j in range(i+1, M+N):
+                        yj = XAll[j]
+                        D[i, j] = method(xi, yj)
+                D = D + D.T
+                sio.savemat(filename, {"D":D})
+                print("Elapsed Time: {:.3f}".format(time.time()-tic))
+
+if __name__ == '__main__':
+    circular=False
+    all_eps = np.arange(0, 11)/10
+    methods = {}
+    methods["dope"] = lambda X, Y: dope_match(X[1], Y[1], circular=circular)[0]
+    methods["bottleneck"] = lambda X, Y: gudhi.bottleneck_distance(X[0].PD, Y[0].PD)
+    methods["wasserstein"] = lambda X, Y: wasserstein(X[0].PD, Y[0].PD)
+    
+
+    parser = argparse.ArgumentParser(description="Evaluating UCR dataset",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("-p", "--path", type=str, action="store", default="./results", help="Path to results")
+    parser.add_argument("-i", '--index', type=int, action="store", help="UCR dataset index")
+    cmd_args = parser.parse_args()
+
+    dataset_name = pyts.datasets.ucr_dataset_list()[cmd_args.index]
+    dataset = get_dataset(dataset_name, all_eps, circular=False)
+    print("Doing", dataset_name)
+    get_dataset_distances(dataset_name, dataset, methods, cmd_args.path)
